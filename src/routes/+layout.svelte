739be226b992949188c15e413cafcfc913405801
David <sanderdj90@gmail.com>
2025-01-25 20:40:10 -0700

    scaffold pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,5 +2,7 @@
 	import '../app.css';
 	let { children } = $props();
 </script>
+<a  href="/workouts" id="workout">Workouts</a>
+<a href="/exercises" id="exercises">Exercises</a>
 
 {@render children()}
