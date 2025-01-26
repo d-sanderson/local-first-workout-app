5b126d9f1b5db7f045163436b29d3adac10098e4
David <sanderdj90@gmail.com>
2025-01-26 13:34:42 -0700

    refactor: remove numeric id, replace with uuid

--- src/routes/api/sync-exercises/+server.ts
+++ src/routes/api/sync-exercises/+server.ts
@@ -3,12 +3,19 @@ import type { Exercise } from "$lib/types.js";
 import { json } from "@sveltejs/kit";
 
 export async function POST({ request }) {
-  const payload = await request.json();
+  const { userId, exercises } = await request.json();
 
-  const promises = payload.exercises.map((exercise: Exercise) =>
-    supabase.from('exercises').upsert({ ...exercise, userId: payload.userId })
-  );
+  const promises = exercises.map((exercise: Exercise) => {
+    // remove id, it will be generated as a unique uuid by supabase
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { id, ...rest } = exercise;
+    return supabase
+      .from('exercises')
+      .upsert({ ...rest, userId })
+  });
 
   const results = await Promise.allSettled(promises);
+  console.log(results)
   return json({ status: 200, results });
 }
+
